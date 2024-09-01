474fff991ae0fe68a409e3eb8648a2f05f9a0ff7
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-09-01 11:34:35 -0300

    Criação das rotas get de Produto conforme o vídeo da aula 2

--- Minimal-API/Program.cs
+++ Minimal-API/Program.cs
@@ -1,6 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Minimal_API.DataBase;
+using Minimal_API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Context>(opt => opt.UseInMemoryDatabase("Produtos"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/Produtos", async (Context db) =>
+await db.Produtos.ToListAsync<Produto>()
+);
+
+app.MapGet("/Produtos/EmEstoque", async (Context db) =>
+await db.Produtos.Where(p => p.quatidadeEmEstoque >= 1).ToListAsync<Produto>()
+);
+
+app.MapGet("/Produtos/ForaDeEstoque", async (Context db) =>
+await db.Produtos.Where(p => p.quatidadeEmEstoque == 0).ToListAsync<Produto>()
+);
+
+app.MapGet("/Produtos/{id}", async (Context db, int id) =>
+    await db.Produtos.FindAsync(id)
+    is Produto produto
+    ? Results.Ok(produto)
+    : Results.NotFound()
+);
 
 app.Run();
